6e8f59891c93e4d0dd038fac6f397d1b8f0ec549
Olamide <114658879+olamide9393@users.noreply.github.com>
2023-12-07 01:27:11 +0100

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -237,7 +237,7 @@ const Navbar = () => {
         </div>
       </nav>
       <Outlet />
-      <br />
+      
       <div style={{ border: "1px solid green" }}></div>
       <br />
       <Footer />
